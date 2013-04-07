c10c5e518309150e22b302086b77563500868ff5
Ruanae <ruanae@gmail.com>
2013-04-07 23:19:35 +0100

    Updated POM

--- src/main/java/co/uk/flyingpiglabs/App.java
+++ src/main/java/co/uk/flyingpiglabs/App.java
@@ -1,13 +1,17 @@
 package co.uk.flyingpiglabs;
 
+import org.hibernate.Session;
+
+import co.uk.flyingpiglabs.projectnom.util.HibernateUtil;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+	}
 }
